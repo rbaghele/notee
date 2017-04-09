2b18be577ad11018e7b678433079fdf9feda2daf
ravib <ravib@alohatechnology.com>
2017-04-09 11:14:51 +0530

    Article CRUD

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,40 @@
 class ArticlesController < ApplicationController
 	before_action :set_user
+	before_action :set_article, only: [:edit, :update]
 
 	def index
 		@articles = @user.articles
+	end
+
+	def create
+		article = @user.articles.build(article_params)
+		
+		if article.save
+			redirect_to edit_article_path(article)
+		else
+			redirect_to articles_path
+		end
+	end
+
+	def edit
+	end
+
+	def update
+		if @article.update_attributes(article_params)
+			redirect_to articles_path
+		else
+			render 'edit'
+		end
+	end
+
+	# PRIVATE METHODS
+	private
+	
+	def article_params
+		params.require(:article).permit(:title, :address, :category_id, :tags, :content)
+	end
+
+	def set_article
+		@article = Article.find(params[:id])
 	end
 end
